0031d6986aaf3ffe98df87fc7e1af519178462b9
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-08-04 18:23:56 +0530

    Added Oauth to feedback form

--- src/components/inputComponents/InputField.js
+++ src/components/inputComponents/InputField.js
@@ -1,73 +1,79 @@
-import { StyleSheet, Text, View,TextInput } from 'react-native'
-import React from 'react'
-import {MicrophoneIcon} from 'react-native-heroicons/outline'
-const InputField = ({ multi,inputname,onChangeText,name,placeholder }) => {
-    
-  return (
-    <View style={styles.container}>
-      <Text style={styles.inputLabel}> {inputname}</Text>
-      {multi?(<View style={styles.filterWrapper}><TextInput
-      style={styles.commentFieldStyle}
-        multiline
-        name={name}
-        onChangeText={onChangeText}
-        placeholder={placeholder}
-        ></TextInput>
-        <MicrophoneIcon style={styles.microIcon} size={23} color="#8E8E8E" />
-        </View>):
-       ( <TextInput
-        style={styles.inputFieldStyle}
-        name={name}
-        onChangeText={onChangeText}
-        placeholder={placeholder}
-        ></TextInput>)}
-        
-    </View>
-  )
-}
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from "react";
+import { MicrophoneIcon } from "react-native-heroicons/outline";
+const InputField = ({ multi, inputname, onChangeText, name, placeholder }) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.inputLabel}> {inputname}</Text>
+            {multi ? (
+                <View style={styles.filterWrapper}>
+                    <TextInput
+                        style={styles.commentFieldStyle}
+                        multiline
+                        name={name}
+                        onChangeText={onChangeText}
+                        placeholder={placeholder}
+                    ></TextInput>
+                    <MicrophoneIcon
+                        style={styles.microIcon}
+                        size={23}
+                        color="#8E8E8E"
+                    />
+                </View>
+            ) : (
+                <TextInput
+                    style={styles.inputFieldStyle}
+                    name={name}
+                    onChangeText={onChangeText}
+                    placeholder={placeholder}
+                ></TextInput>
+            )}
+        </View>
+    );
+};
 
-export default InputField
+export default InputField;
 
 const styles = StyleSheet.create({
-    container:{
-        margin:8,
+    container: {
+        margin: 8,
     },
-    inputLabel:{
-        color:"#8E8E8E",
-        fontSize:14,
-        paddingBottom:10
+    inputLabel: {
+        color: "#8E8E8E",
+        fontSize: 14,
+        paddingBottom: 10,
     },
-    inputFieldStyle:{
-        width:"100%",
-        height:50,
-        backgroundColor:"#C8CACB",
-        borderRadius:4,
-        color:"#8E8E8E",
-        paddingLeft:20,
+    inputFieldStyle: {
+        width: "100%",
+        height: 50,
+        backgroundColor: "#C8CACB",
+        borderRadius: 4,
+        color: "#8E8E8E",
+        paddingLeft: 20,
     },
-    commentFieldStyle:{
+    commentFieldStyle: {
         flex: 1,
-        height:116,
-        width:"100%",
-        paddingLeft:20,
-        paddingBottom:60,
-        fontSize:12,
-        color:"#8E8E8E",
-        borderWidth:1,
-        borderRadius:4
+        height: 116,
+        width: "100%",
+        paddingLeft: 20,
+        paddingBottom: 60,
+        fontSize: 12,
+        color: "#8E8E8E",
+        borderWidth: 1,
+        borderRadius: 4,
     },
     filterWrapper: {
         flexDirection: "row",
         alignItems: "center",
-        borderColor: '#D0D0D0',
+        borderColor: "#D0D0D0",
         borderWidth: 1,
         borderRadius: 5,
     },
-   
+
     microIcon: {
-        position: 'absolute',
+        position: "absolute",
         right: 20,
-        top:20,
-        color:"#2C81E0",
+        top: 20,
+        color: "#2C81E0",
     },
-})
+});
