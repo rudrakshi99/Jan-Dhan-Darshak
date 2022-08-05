8511bfa30a96f1096d0ac85759a0324c95f6f265
Anmol Srivastava <anmol.srivastava@createbytes.com>
2022-08-05 20:02:36 +0530

    added fields in the user model

--- jan_dhan_darshak/users/models.py
+++ jan_dhan_darshak/users/models.py
@@ -1,5 +1,6 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, TextChoices, UUIDField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -11,11 +12,23 @@ class User(AbstractUser):
     check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
 
+    class UserType(TextChoices):
+        ADMIN = "AD", _("ADMIN")
+        CUSTOMER = "CU", _("CUSTOMER")
+        SALES_PERSON = "SP", _("SALES PERSON")
+
     #: First and last name do not cover name patterns around the globe
+    uuid = UUIDField(default=uuid.uuid4, editable=False, db_index=True)
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
-
+    user_type = CharField(
+        max_length=20,
+        choices=UserType.choices,
+        default=UserType.CUSTOMER,
+    )
+    phone_number = CharField(max_length=10, null=True, unique=True)
+    
     def get_absolute_url(self):
         """Get url for user's detail view.
 
