0d5a44bbd8272c8d66449eae8668246856861bb0
ParthSharmaT <parthsharmatanguriya@gmail.com>
2022-08-13 23:05:02 +0530

    response_payloads in MissingSuggestions Added

--- jan_dhan_darshak/missing_suggestions/api/views.py
+++ jan_dhan_darshak/missing_suggestions/api/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from jan_dhan_darshak.core.utils import response_payload
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 from jan_dhan_darshak.missing_suggestions.models import MissingSuggestions
 from .serializers import MissingSuggestionsSerializer
 # Create your views here.
@@ -30,25 +31,16 @@ class missingSuggestion(APIView):
             serializer= MissingSuggestionsSerializer(data=data)
             if serializer.is_valid():
                 serializer.save()
-                return Response({
-                'status': True,
-                'message': 'success data',
-                'data': serializer.data,
-                })
-            return Response({
-                'status': False,
-                'message': 'invalid data',
-                'data': serializer.errors,
-            })
+                return Response(
+                response_payload(success=True, data=serializer.data, msg="Thank you for the Suggestion!"),status=status.HTTP_200_OK,)
+            return Response(
+                response_payload(success=False, data=serializer.errors, msg="Invalid Data!"),status=status.HTTP_400_BAD_REQUEST,)
         except Exception as e:
-            return Response({
-                'status': 400,
-                'message': 'Bad Request',
-                'error': str(e)
-            })
+            return Response(
+                response_payload(success=False, data=str(e), msg="error while creating!"),status=status.HTTP_400_BAD_REQUEST,)
 
 #Get suggestion by id
-    #Request Body: {
+    # Request Body: {
     #     "uid": "0cd45845-8a85-4221-a5cf-2419566b0801"
     # }
 class trackSuggestion(APIView):
@@ -59,17 +51,13 @@ class trackSuggestion(APIView):
             uid=request.data['uid']
             data=MissingSuggestions.objects.get(uid=uid)
             serializer=MissingSuggestionsSerializer(data)
-            return Response({
-            'status': True,
-            'message': 'success data',
-            'data': serializer.data,
-            })
+
+            return Response(
+                response_payload(success=True, data=serializer.data, msg="Thanks for the feedback."),status=status.HTTP_200_OK,)
+                          
         except Exception as e:
-            return Response({
-                'status': 400,
-                'message': 'Bad Request',
-                'error': f"Suggestion not found with the track id- {request.data['uid']}"
-            })
+            return Response(
+                response_payload(success=False, msg=f"Suggestion not found with the track id : {request.data['uid']}"),status=status.HTTP_400_BAD_REQUEST,)
 
 
 #get all suggestions by user id
@@ -81,16 +69,12 @@ class groupTrack(APIView):
     def post(self,request):
         try:
             User=request.data['User']
-            data=MissingSuggestions.objects.get(User=User)
-            serializer=MissingSuggestionsSerializer(data)
-            return Response({
-            'status': True,
-            'message': 'success data',
-            'data': serializer.data,
-            })
+            data=MissingSuggestions.objects.filter(User=User)
+            print(data)
+            serializer=MissingSuggestionsSerializer(data,many=True)
+            return Response(
+                response_payload(success=True, data=serializer.data, msg="Thanks for the feedback."),status=status.HTTP_200_OK,)
+        
         except Exception as e:
-            return Response({
-                'status': 400,
-                'message': 'Bad Request',
-                'error': f"Suggestion not found for User Id- {request.data['User']}"
-            })
+            return Response(
+                response_payload(success=False, msg= f"Suggestion not found for User Id- {request.data['User']}"),status=status.HTTP_400_BAD_REQUEST,)
