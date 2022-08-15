34f533a6f5ceba2c3d9c0ffa6ee37c5cec200c55
Anmol Srivastava <anmol.srivastava@createbytes.com>
2022-08-15 15:03:54 +0530

    commented out unused urls

--- jan_dhan_darshak/users/urls.py
+++ jan_dhan_darshak/users/urls.py
@@ -10,9 +10,9 @@ from jan_dhan_darshak.users.views import (
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
+    # path("~redirect/", view=user_redirect_view, name="redirect"),
+    # path("~update/", view=user_update_view, name="update"),
+    # path("<str:username>/", view=user_detail_view, name="detail"),
     path("signup/", view=UserLoginViewset.as_view({"post": "create"}), name="signup"),
     path("verify/", view=UserLoginViewset.as_view({"post": "verify"}), name="verify"),
 ]
