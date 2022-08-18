58ff3cc65b1f3b7e6e28ebc60fcd08eb3ea3dd14
Anmol Srivastava <anmol.srivastava@createbytes.com>
2022-08-18 11:37:49 +0530

    added refresh token api

--- config/api_router.py
+++ config/api_router.py
@@ -3,13 +3,27 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from jan_dhan_darshak.users.api.views import UserViewSet
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from django.urls import path
+
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-router.register("users", UserViewSet)
+# router.register("users", UserViewSet)
 
 
 app_name = "api"
 urlpatterns = router.urls
+
+
+urlpatterns += [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
